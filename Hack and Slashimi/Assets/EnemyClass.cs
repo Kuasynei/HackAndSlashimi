1332b15b7a42b6514d77a9ed417a91ea899b1939
Kuasynei <kuasynei@gmail.com>
2016-05-06 13:33:24 -0400

    Initial Commit - Tony
    please no break

---
+++ Hack and Slashimi/Assets/EnemyClass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : Entity {
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
---
+++ Hack and Slashimi/Assets/EntityClass.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity
+{
+	protected float health = 1;
+
+	protected float takeDamage(float damage)
+	{
+		health -= damage;
+		return health;
+	}
+
+	protected float heal(float heal)
+	{
+		heal += heal;
+		return health;
+	}
+}
