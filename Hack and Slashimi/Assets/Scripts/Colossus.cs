bb5360da854213a3bad787c551f5003046d910ed
Matthew Ferreiro <matthew.ferreiro@hotmail.com>
2016-05-06 19:42:55 -0400

    Colossus and Gate
    Colossus moves and stops when there is a gate in the way

---
+++ Hack and Slashimi/Assets/Scripts/Colossus.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Colossus : EntityClass 
+{
+	public float movementSpeed;
+	[SerializeField] float maxHealth = 100;
+	bool gateToDestroy;
+	Rigidbody rB;
+
+	void Awake () 
+	{
+		health = maxHealth;
+		rB = GetComponent<Rigidbody> ();
+	}
+
+	void Update()
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, Vector3.right, out hit, 1.5f))
+		{
+			gateToDestroy = true;
+			rB.velocity = new Vector3(0,0,0);
+			Debug.DrawLine(transform.position, hit.point);
+		}
+		else
+		{
+			gateToDestroy = false;
+		}
+	}
+
+	void FixedUpdate () 
+	{
+		if(!gateToDestroy)
+		{
+			rB.velocity = new Vector3(movementSpeed / 10,0,0);
+		}
+	}
+}
