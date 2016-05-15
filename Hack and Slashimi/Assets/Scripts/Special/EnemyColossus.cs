9ac0cde436657955f9b8d64d1f1f053a907f787f
Matt <matthew.ferreiro@hotmail.com>
2016-05-14 22:31:58 -0400

    BossColoss Health and Shockwave
    Health is kinda functional now? But shockwave is WIP being implemented.

--- Hack and Slashimi/Assets/Scripts/Special/EnemyColossus.cs
+++ Hack and Slashimi/Assets/Scripts/Special/EnemyColossus.cs
@@ -4,19 +4,51 @@ using System.Collections;
 public class EnemyColossus : EntityClass 
 {
 	[SerializeField] float maxHealth = 10000;
+	[SerializeField] float shockwaveTime = 5.0f;
+	[SerializeField] GameObject shockwaveObject;
+	GameObject player;
+	bool gotPlayer;
 
 	void Awake () 
 	{
 		maxH = maxHealth;
 	}
 
+//	void Start()
+//	{
+//		player = GameManager.GetPlayer();
+//	}
 	void Update()
 	{
-		Debug.Log(gameObject.name + "'s Health: " + health);
+		if(!gotPlayer)
+		{
+			player = GameManager.GetPlayer();
+			if(player != null)
+			{
+				gotPlayer = true;
+				//Debug.Log("Player Received!");
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+		//Debug.Log(Vector3.Distance(transform.position, player.transform.position));
 
 		if(health <= 0)
 		{
 			Destroy(gameObject);
+		}
+
+		if(Vector3.Distance(transform.position, player.transform.position) < 20.0f)
+		{
+			shockwaveTime += Time.deltaTime;
+
+			if(shockwaveTime > 5)
+			{
+				Debug.Log("SHOCKWAVE");
+				shockwaveTime = 0;
+			}
 		}
 	}
 }
